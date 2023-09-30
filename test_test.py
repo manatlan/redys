fd9f1f47a50701c61157c1922773d56406a5da56
manatlan <manatlan@gmail.com>
2023-09-30 08:33:37 +0200

    pic: test pytest

--- test_test.py
+++ test_test.py
@@ -1,4 +1,16 @@
 import pytest
+import asyncio
+import redys
 
-def test_test():
-    assert 1==1
+@pytest.fixture()
+def server():
+    print("tearup")
+    asyncio.ensure_future( redys.Server() )
+    yield "resource"
+    print("teardown")
+
+async def test_fun( server ):
+    with redys.AClient() as bus:
+        await bus.set("val",42)
+        x = await bus.get("val")
+        assert x == 42
