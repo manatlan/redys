253e09bbf6800fd7fcd858b2579a4dc12bfcd6c1
manatlan <manatlan@gmail.com>
2023-09-30 09:01:12 +0200

    test: add test sync

---
+++ test_sync.py
@@ -0,0 +1,16 @@
+import pytest
+import asyncio
+import redys
+
+@pytest.fixture()
+def server():
+    print("tearup")
+    asyncio.ensure_future( redys.Server() )
+    yield "resource"
+    print("teardown")
+
+@pytest.mark.asyncio
+async def test_ping( server ):
+    with redys.Client() as bus:
+        x = bus.ping()
+        assert x == "pong"
