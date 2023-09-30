6813bbd1d2de98e941e7f367e14447bc3704c070
manatlan <manatlan@gmail.com>
2023-09-30 08:55:44 +0200

    test: add ping test

--- test_test.py
+++ test_test.py
@@ -10,8 +10,24 @@ def server():
     print("teardown")
 
 @pytest.mark.asyncio
-async def test_fun( server ):
+async def test_ping( server ):
     with redys.AClient() as bus:
-        await bus.set("val",42)
+        x = await bus.ping()
+        assert x == "pong"
+
+@pytest.mark.asyncio
+async def test_get_set( server ):
+    with redys.AClient() as bus:
+        x = await bus.get("val")
+        assert x is None
+
+        assert await bus.set("val",42)
+
         x = await bus.get("val")
         assert x == 42
+
+        o=dict(a=42,s="hello",pi=3.14)
+        assert await bus.set("val",o)
+
+        x = await bus.get("val")
+        assert x == o
